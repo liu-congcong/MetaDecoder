1de6889cf52a71adb83358e569bc871c9c60257c
Liucongcong <liu_congcong@icloud.com>
2024-12-07 21:32:15 +0800

    Update metadecoder_coverage.py

--- metadecoder/metadecoder_coverage.py
+++ metadecoder/metadecoder_coverage.py
@@ -1,110 +1,100 @@
-import os
+from ctypes import c_int64
 from datetime import datetime
 from math import ceil
-from multiprocessing import Pool
-from re import compile
+from multiprocessing import Process, Queue
+from multiprocessing.sharedctypes import Value
 
 import numpy
-from .make_file import make_file
-from .sam_utility import generate_block, read_sam_file, read_sam_header
+from threadpoolctl import threadpool_limits
+
+from .bam import getUngappedRegions, indexBam, readIndices
+from .plot import plotBar
 
 
-def get_coverage(input_file, block_start, block_end, output_file, bin_size, mapq, aligned_threshold):
-    template = compile('(\d+)(\D)')
-    sequence2bin_coverages = dict()
-    regions = list()
-    for _, sequence_id, position, cigar in read_sam_file(input_file, block_start, block_end, mapq):
-        bin_index = ceil(position / bin_size)
-        # Determine the valid read. #
-        aligned_length = 0  # Number of matches and mismatches. #
-        total_length = 0
-        for cigar_ in template.finditer(cigar):
-            operations = int(cigar_.group(1))
-            operation = cigar_.group(2)
-            if operation in '=MX': # Consume reference. #
-                aligned_length += operations
-                regions.append((position, position + operations - 1))
-                position += operations
-            elif operation in 'DN': # Consume reference. #
-                position += operations
-            total_length += operations
-        if aligned_length / total_length >= aligned_threshold: # Valid read. #
-            for region_start, region_end in regions:
-                while True:
-                    if region_end <= bin_index * bin_size: # bin_start <= region_start <= region_end <= bin_end #
-                        sequence2bin_coverages[(sequence_id, bin_index)] = sequence2bin_coverages.get((sequence_id, bin_index), 0) + region_end - region_start + 1
-                        break
-                    elif region_start <= bin_index * bin_size: # bin_start <= region_start <= bin_end < region_end #
-                        sequence2bin_coverages[(sequence_id, bin_index)] = sequence2bin_coverages.get((sequence_id, bin_index), 0) + bin_index * bin_size - region_start + 1
-                        region_start = bin_index * bin_size + 1
-                        bin_index += 1
-                    else: # bin_start <= bin_end < region_start <= region_end #
-                        bin_index += 1
-        regions.clear()
-    open_file = open(output_file, 'w')
-    for (sequence, bin_index), coverage in sequence2bin_coverages.items():
-        open_file.write('\t'.join([sequence, str(bin_index - 1), str(coverage)]) + '\n')
-    open_file.close()
+def workerProcess(queue, files, mapq, identity, binSize, output, n, N):
+    '''
+    queue: queue
+    files: list
+    mapq: int
+    identity: float
+    binSize: int
+    output: string
+    n: int
+    N: Value(c_int64, 0)
+    '''
+    m = len(files)
+    while True:
+        sequence, length, fileOffsets, dataOffsets, dataSizes = queue.get()
+        if sequence is None:
+            break
+        x = numpy.zeros(shape = (ceil(length / binSize), m), dtype = numpy.float64)
+        binSizes = numpy.full(shape = x.shape[0], fill_value = binSize, dtype = numpy.int64)
+        binSizes[-1] = length % binSize
+        if binSizes[-1] == 0:
+            binSizes[-1] = binSize
+        for i, (file, fileOffset, dataOffset, dataSize) in enumerate(zip(files, fileOffsets, dataOffsets, dataSizes)):
+            for position, regions in getUngappedRegions(file, fileOffset, dataOffset, dataSize, mapq, identity):
+                binIndex = ceil(position / binSize)
+                for regionStart, regionEnd in regions:
+                    while True:
+                        if regionEnd <= binIndex * binSize: # binStart <= regionStart <= regionEnd <= binEnd #
+                            x[binIndex - 1, i] += regionEnd - regionStart + 1
+                            break
+                        elif regionStart <= binIndex * binSize: # binStart <= regionStart <= binEnd < regionEnd #
+                            x[binIndex - 1, i] += binIndex * binSize - regionStart + 1
+                            regionStart = binIndex * binSize + 1
+                            binIndex += 1
+                        else: # binStart <= binEnd < regionStart <= regionEnd #
+                            binIndex += 1
+        x /= binSizes[ : , None]
+        N.acquire()
+        openFile = open(output, 'a')
+        for i, (binSizeI, xi) in enumerate(zip(binSizes, x), start = 1):
+            xi = '\t'.join(xi.astype(numpy.str_).tolist())
+            openFile.write(f'{sequence}\t{i}\t{binSizeI}\t{xi}\n')
+        openFile.close()
+        N.value += 1
+        plotBar(N.value / n)
+        N.release()
+    return None
+
+
+def createProcesses(files, n, mapq, identity, binSize, threads, output):
+    N = Value(c_int64, 0)
+    queue = Queue()
+    processes = list()
+    with threadpool_limits(limits = 1):
+        for i in range(threads):
+            processes.append(Process(target = workerProcess, args = (queue, files, mapq, identity, binSize, output, n, N)))
+            processes[-1].start()
+    return (queue, processes, N)
+
+
+def freeProcesses(queue, processes):
+    for process in processes:
+        queue.put((None, None, None, None, None))
+    queue.close()
+    queue.join_thread()
+    for process in processes:
+        process.join()
+        process.close()
     return None
 
 
 def main(parameters):
 
-    sam_files = len(parameters.sam)
-    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '->', 'Reading header sections from all sam files.', flush = True)
-    sequence_struct = dict()
-    for sam_file in parameters.sam:
-        for sequence, sequence_length in read_sam_header(sam_file):
-            sequence_struct[(sequence, sequence_length)] = sequence_struct.get((sequence, sequence_length), 0) + 1
-    for (sequence, sequence_length), pairs in sequence_struct.items():
-        assert pairs == sam_files, f"All sam files should have the same header, but sequence \"{sequence}\" is not present in all files."
+    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} -> Indexing all bam files.', flush = True)
+    indexBam(parameters.bam, parameters.threads)
 
-    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '->', 'Loading sam files.', flush = True)
-    file2temp_files = dict()
-    process_pool = Pool(os.cpu_count())
-    for file in parameters.sam:
-        for block_start, block_end in generate_block(file, parameters.threads):
-            temp_file = make_file()
-            file2temp_files.setdefault(file, list()).append(temp_file)
-            process_pool.apply_async(
-                get_coverage,
-                (
-                    file,
-                    block_start,
-                    block_end,
-                    temp_file,
-                    parameters.bin_size,
-                    parameters.mapq,
-                    parameters.aligned
-                )
-            )
-    process_pool.close()
-    process_pool.join()
-    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '->', 'Done.', flush = True)
+    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} -> Loading all index files.', flush = True)
+    sequences, lengths, fileOffsets, dataOffsets, dataSizes = readIndices(parameters.bam, parameters.threads)
 
-    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '->', 'Writing to file.', flush = True)
-    sequence2bin_coverages = dict()
-    sequence2bin_sizes = dict()
-    for sequence, sequence_length in sequence_struct:
-        sequence2bin_coverages[sequence] = numpy.zeros(shape = (ceil(sequence_length / parameters.bin_size), sam_files), dtype = numpy.int64)
-        sequence2bin_sizes[sequence] = numpy.array(
-            [
-                min(parameters.bin_size * (bin_index + 1), sequence_length) - parameters.bin_size * bin_index for bin_index in range(ceil(sequence_length / parameters.bin_size))
-            ],
-            dtype = numpy.int64
-        )
-    for coverage_index, temp_files in enumerate(file2temp_files.values()):
-        for temp_file in temp_files:
-            open_file = open(temp_file, 'r')
-            for line in open_file:
-                lines = line.rstrip('\n').split('\t')
-                sequence2bin_coverages[lines[0]][int(lines[1]), coverage_index] += float(lines[2])
-            open_file.close()
-            os.remove(temp_file)
-    open_file = open(parameters.output, 'w')
-    open_file.write('\t'.join(['sequence id', 'bin index', 'bin size'] + ['coverage' + str(coverage_index + 1) for coverage_index in range(sam_files)]) + '\n')
-    for sequence, BinCoverages in sequence2bin_coverages.items():
-        for bin_index, (bin_size, bin_coverage) in enumerate(zip(sequence2bin_sizes[sequence], BinCoverages), start = 1):
-            open_file.write('\t'.join([sequence, str(bin_index), str(bin_size)] + (bin_coverage / bin_size).astype(numpy.str_).tolist()) + '\n')
-    open_file.close()
-    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '->', 'Finished.', flush = True)
+    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} -> Computing coverage.', flush = True)
+    openFile = open(parameters.output, 'w')
+    openFile.write('\t'.join(['sequence id', 'bin index', 'bin size'] + ['coverage' + str(coverage_index + 1) for coverage_index in range(len(parameters.bam))]) + '\n')
+    openFile.close()
+    queue, processes, N = createProcesses(parameters.bam, len(sequences), parameters.mapq, parameters.aligned, parameters.bin_size, parameters.threads, parameters.output)
+    for sequence, length, fileOffset, dataOffset, dataSize in zip(sequences, lengths, fileOffsets, dataOffsets, dataSizes):
+        queue.put((sequence, length, fileOffset, dataOffset, dataSize))
+    freeProcesses(queue, processes)
+    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} -> Finished.', flush = True)
